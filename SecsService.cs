ced25a1201a0dd1790e19b2d1b7f451b42dada2e
oruparu <ks00141@naver.com>
2022-02-27 19:17:11 +0900

    Service Class Init

---
+++ SecsService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Secs4Net;
+
+namespace ARMS
+{
+    class SecsService
+    {
+        SecsGem driver;
+        SecsMsgController msgAdmin;
+
+        public SecsService(Form1 form)
+        {
+            driver = new SecsGem(false, IPAddress.Any, 5717);
+            driver.PrimaryMessageReceived += msgReceived;
+            msgAdmin = new SecsMsgController(driver, form);
+        }
+
+        public void start()
+        {
+            driver.Start();
+        }
+
+        void msgReceived(object sender, PrimaryMessageWrapper pMsg)
+        {
+            msgAdmin.msgReceive(pMsg);
+        }
+    }
+}
