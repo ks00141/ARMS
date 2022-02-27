8fd8ffa1e47346e8d5ee567b139ce6a25c0136ad
oruparu <ks00141@naver.com>
2022-02-27 19:18:00 +0900

    SecsGem Message Interface

---
+++ S6F11InterFace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Secs4Net;
+
+namespace ARMS
+{
+    interface S6F11InterFace
+    {
+        SecsMessage sendSuc();
+        SecsMessage sendFail();
+    }
+}
