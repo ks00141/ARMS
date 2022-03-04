297f726cc5c49ff9244173949dc3e4c34484bfa7
oruparu <ks00141@naver.com>
2022-03-04 16:17:12 +0900

    S2F41 Receiver Interface

---
+++ S2F41ReceiverInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Secs4Net;
+
+namespace ARMS
+{
+    interface S2F41ReceiverInterface
+    {
+        bool checkRCMD();
+        String getRCMD();
+        void replyS2F42(bool flag);
+        List<String> getParams();
+    }
+}
