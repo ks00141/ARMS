d75cfb3160113c228aba068e620b277f368fcd72
oruparu <ks00141@naver.com>
2022-03-04 16:15:08 +0900

    message controller object factory

---
+++ MsgCtrFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Secs4Net;
+
+namespace ARMS
+{
+    class MsgCtrFactory:MsgCtrFactoryInterface
+    {
+
+        public MsgCtrInterface getController(SecsGem driver, dynamic form)
+        {
+            return new SecsMsgController(driver, form);
+        }
+    }
+}
