e5018eea3ee16b97d6d8aa69c77cc18eb09d5cbd
oruparu <ks00141@naver.com>
2022-03-14 17:00:31 +0900

    DAO 작성

---
+++ DAO/JobDAO.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Secs4Net;
+using ARMS.Model;
+using System.Data;
+
+namespace ARMS.DAO
+{
+    class JobDAO
+    {
+        Entity entity;
+        DataTable recipeParams;
+
+        public JobDAO()
+        {
+
+
+            recipeParams = new DataTable();
+
+            recipeParams.Columns.Add("cluster_recipe", typeof(string));
+            recipeParams.Columns.Add("frontside_recipe", typeof(string));
+            recipeParams.Columns.Add("inspection_dies", typeof(string));
+            recipeParams.Columns.Add("inspection_columns", typeof(string));
+            recipeParams.Columns.Add("inspection_rows", typeof(string));
+
+        }
+
+        public Entity selectQuery(PrimaryMessageWrapper pMsg)
+        {
+            Item items = pMsg.Message.SecsItem.Items[1].Items[0].Items[3];
+
+            DataRow row = recipeParams.NewRow();
+            row["cluster_recipe"] = pMsg.Message.SecsItem.Items[1].Items[0].Items[0].GetValue<String>();
+            foreach (Item item in items.Items)
+            {
+                if (item.Items[0] == "Frontside/RecipeName") row["frontside_recipe"] = item.Items[1].Items[0].GetValue<String>();
+                if (item.Items[0] == "Frontside/TestableDies") row["inspection_dies"] = item.Items[1].Items[0].GetValue<String>();
+                if (item.Items[0] == "Frontside/ColumnNumber") row["inspection_columns"] = item.Items[1].Items[0].GetValue<String>();
+                if (item.Items[0] == "Frontside/RowNumber") row["inspection_rows"] = item.Items[1].Items[0].GetValue<String>();
+            }
+
+            recipeParams.Rows.Add(row);
+
+            entity = new Entity(
+                recipeParams.Rows[0][0].ToString(),
+                recipeParams.Rows[0][1].ToString(),
+                recipeParams.Rows[0][2].ToString(),
+                recipeParams.Rows[0][3].ToString(),
+                recipeParams.Rows[0][4].ToString()
+                );
+
+
+            return entity;
+        }
+        
+    }
+}
---
+++ DAO/SpecDAO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARMS.Model;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace ARMS.DAO
+{
+    class SpecDAO
+    {
+        MySqlConnection conn;
+        Entity entity;
+        String queryForm;
+        public SpecDAO()
+        {
+            conn = new DBConnectorFactory().getConnection();
+            this.queryForm =
+                        @"SELECT
+	                        a.cluster_recipe
+	                        , a.frontside_recipe
+	                        , a.inspection_dies
+	                        , a.inspection_columns
+	                        , a.inspection_rows
+                        FROM 
+	                        recipe.spec a
+                        WHERE 1 = 1
+                            AND a.cluster_recipe ='{0}';";
+        }
+
+        public Entity selectQuery(String clusterRecipe)
+        {
+            String query = String.Format(this.queryForm, clusterRecipe);
+            MySqlDataAdapter adt = new MySqlDataAdapter(query, conn);
+            DataTable specParams = new DataTable();
+            adt.Fill(specParams);
+
+            entity = new Entity(
+                specParams.Rows[0][0].ToString(),
+                specParams.Rows[0][1].ToString(),
+                specParams.Rows[0][2].ToString(),
+                specParams.Rows[0][3].ToString(),
+                specParams.Rows[0][4].ToString()
+                );
+
+            return entity;
+        }
+    }
+}
