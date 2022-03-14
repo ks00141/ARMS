a32473c06cf7a17ee8b944a580fd3ad9ee8b9839
oruparu <ks00141@naver.com>
2022-03-14 16:59:55 +0900

    S1F3 Message Controller

---
+++ Controller/S1F3Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Secs4Net;
+
+namespace ARMS.Controller
+{
+    class S1F3Controller
+    {
+        public void req(PrimaryMessageWrapper pMsg)
+        {
+            pMsg.ReplyAsync(
+                new SecsMessage(
+                    1,
+                    4,
+                    "S1F4",
+                    Item.L(
+                        Item.A(
+                            "ARMS"
+                        )
+                    )
+                )
+            );
+        }
+    }
+}
