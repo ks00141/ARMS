bff16b3759ec6e4eef578803ff945e8f77fa7559
oruparu <ks00141@naver.com>
2022-03-14 17:00:42 +0900

    model 작성

---
+++ Model/Entity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARMS.Model
+{
+    class Entity
+    {
+
+        private String clusterRecipe;
+        private String frontsideRecipe;
+        private String inspectionDies;
+        private String inspectionColumns;
+        private String inspectionRows;
+        public Entity(String clusterRecipe, String frontsideRecipe, String inspectionDies, String inspectionColumns, String inspectionRows)
+        {
+            this.clusterRecipe = clusterRecipe;
+            this.frontsideRecipe = frontsideRecipe;
+            this.inspectionDies = inspectionDies;
+            this.inspectionColumns = inspectionColumns;
+            this.inspectionRows = inspectionRows;
+        }
+
+        public String GetClusterRecipe()
+        {
+
+            return this.clusterRecipe;
+        }
+
+        public String GetFrontsideRecipe()
+        {
+
+            return this.frontsideRecipe;
+        }
+
+        public String GetInspectionDies()
+        {
+
+            return this.inspectionDies;
+        }
+
+        public String GetInspecionColumns()
+        {
+
+            return this.inspectionColumns;
+        }
+
+        public String GetInspecionRows()
+        {
+
+            return this.inspectionRows;
+        }
+    }
+}
