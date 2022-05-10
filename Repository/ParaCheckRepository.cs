5a7cbda721e8c6ac85c0e5f4d138d282f23632e7
심귀 한짝 <ks00141@outlook.kr>
2022-05-10 22:14:49 +0900

    S2F41(RCMD:Recipe Para Check) Repository

---
+++ Repository/ParaCheckRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARMS.Model;
+using Secs4Net;
+
+namespace ARMS.Repository
+{
+    class ParaCheckRepository
+    {
+        public SecsMessage S2F42()
+        {
+            S2F42 s2f42 = new S2F42
+            {
+                HCACK = 0
+            };
+
+            return s2f42.Message;
+        }
+    }
+}
