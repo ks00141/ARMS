69da53770896d2d24ea1a09112aa79bede4a664d
심귀 한짝 <ks00141@outlook.kr>
2022-05-10 22:10:00 +0900

    Establish Communication Request Gem Repository

---
+++ Repository/ECRRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARMS.Model;
+using Secs4Net;
+
+//ECR = Establish Communication Request
+
+namespace ARMS.Repository
+{
+    class ECRRepository
+    {
+        
+        public SecsMessage S1F14()
+        {
+            return this.S1F14(0);
+        }
+
+        public SecsMessage S1F14(byte comack)
+        {
+            S1F14 s1f14 = new S1F14
+            {
+                COMACK = comack,
+                MDLN = "ARMS",
+                SOFTREV = "0.1.0"
+            };
+
+            return s1f14.Message;
+        }
+    }
+}
