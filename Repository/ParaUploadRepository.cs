566c1b89d642b32cb5c9f2ff48849af5c4c50180
oruparu <ks00141@naver.com>
2022-05-12 23:11:43 +0900

    para upload

--- Repository/ParaUploadRepository.cs
+++ Repository/ParaUploadRepository.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ARMS.Model;
 using Secs4Net;
+using MySql.Data.MySqlClient;
 
 namespace ARMS.Repository
 {
@@ -12,11 +13,13 @@ namespace ARMS.Repository
     {
         readonly RecipeParam param;
         readonly PrimaryMessageWrapper pMsg;
+        MySqlConnection conn;
 
         public ParaUploadRepository(PrimaryMessageWrapper pMsg)
         {
             param = new RecipeParam();
             this.pMsg = pMsg;
+            conn = new DBConnectorFactory().getConnection();
         }
 
         public RecipeParam GetRecipeParam()
@@ -52,6 +55,31 @@ namespace ARMS.Repository
             };
 
             return s2f42.Message;
+        }
+
+        public void DBUploadParam(RecipeParam param)
+        {
+            string queryForm =
+                @"INSERT INTO recipe.spec
+                    (cluster_reicpe,
+                    frontside_recipe,
+                    inspection_dies,
+                    inspection_columns,
+                    inspeciton_rows)
+                Values(
+                    '{0}', '{1}', '{2}', '{3}', '{4}');";
+            string query = string.Format(queryForm,
+                param.ClusterRecipe,
+                param.FrontsideRecipe,
+                int.Parse(param.InspectionDies),
+                int.Parse(param.InspectionColumns),
+                int.Parse(param.InspectionRows));
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            if (cmd.ExecuteNonQuery() == 1)
+            {
+                Console.WriteLine("Inser Succ");
+            }
         }
     }
 }
