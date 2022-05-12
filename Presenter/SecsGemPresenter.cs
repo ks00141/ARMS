4e3a827fc3ffff99bd8ae1af6c5457fbb5cd766b
오루빠루 <ks00141@naver.com>
2022-05-12 09:19:33 +0900

    SecsGem Presenter

--- Presenter/SecsGemPresenter.cs
+++ Presenter/SecsGemPresenter.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ARMS.Model;
 using ARMS.Service;
+using ARMS.View;
 
 namespace ARMS.Presenter
 {
-    class SecsGemPresenter
+    class SecsGemPresenter:IRecipeParamUPloadView
     {
         readonly SecsGemService service;
 
-        public SecsGemPresenter()
+        public SecsGemPresenter(IRecipeParamUPloadView View)
         {
             this.service = new SecsGemService();
+            this.service.RecipeParamUploadEvent += RecipeParamUpload;
+
+        }
+
+        private void RecipeParamUpload(object sender, RecipeParam e)
+        {
+            ;
         }
 
         public void SecsGemStart()
