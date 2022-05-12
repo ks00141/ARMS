234769a09aa0e1800c42fa5934fdba87993bb13d
심귀 한짝 <ks00141@outlook.kr>
2022-05-12 21:01:02 +0900

    폼이 잘안됨 ㅠㅠ

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,73 +30,49 @@ namespace ARMS
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.btn_recipeList = new System.Windows.Forms.Button();
-            this.btn_config = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 381F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(820, 492);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 114);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(814, 375);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 3;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Controls.Add(this.btn_recipeList, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.btn_config, 1, 0);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 395);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 33);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(814, 94);
-            this.tableLayoutPanel2.TabIndex = 0;
-            // 
-            // btn_recipeList
-            // 
-            this.btn_recipeList.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_recipeList.Location = new System.Drawing.Point(3, 3);
-            this.btn_recipeList.Name = "btn_recipeList";
-            this.btn_recipeList.Size = new System.Drawing.Size(94, 88);
-            this.btn_recipeList.TabIndex = 0;
-            this.btn_recipeList.Text = "Recipe List";
-            this.btn_recipeList.UseVisualStyleBackColor = true;
-            // 
-            // btn_config
-            // 
-            this.btn_config.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_config.Location = new System.Drawing.Point(103, 3);
-            this.btn_config.Name = "btn_config";
-            this.btn_config.Size = new System.Drawing.Size(94, 88);
-            this.btn_config.TabIndex = 1;
-            this.btn_config.Text = "Config";
-            this.btn_config.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 33);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(814, 356);
-            this.panel1.TabIndex = 1;
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(814, 75);
+            this.tableLayoutPanel2.TabIndex = 1;
             // 
             // Form1
             // 
@@ -109,9 +85,7 @@ namespace ARMS
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "ARMS";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -119,10 +93,8 @@ namespace ARMS
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.Button btn_recipeList;
-        private System.Windows.Forms.Button btn_config;
-        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,23 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using MetroSet_UI.Forms;
+﻿using ARMS.Presenter;
 using ARMS.View;
 using log4net;
+using System;
 using System.Reflection;
-using ARMS.Presenter;
+using System.Windows.Forms;
 
 namespace ARMS
 {
-    public partial class Form1 : Form,IPpidListView,IRecipeParamUPloadView
+    public partial class Form1 : Form, IPpidListView, IRecipeParamUPloadView
     {
-        UserControl1 uc1 = new UserControl1();
         SecsGemPresenter secsGemPresenter;
         private static readonly ILog log = LogManager.GetLogger("ARMS/GUI");
 
@@ -41,31 +32,10 @@ namespace ARMS
             {
                 secsGemPresenter.SecsGemStart();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 log.Error($"An exception occurred from {MethodBase.GetCurrentMethod().Name}", e);
             }
-
-
-        }
-
-        public void compare(TextBox a, TextBox b)
-        {
-            if(a.Text != b.Text)
-            {
-                a.ForeColor = Color.Red;
-                b.ForeColor = Color.Red;
-            }
-            else
-            {
-                a.ForeColor = Color.White;
-                b.ForeColor = Color.White;
-            }
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            panel1.Controls.Add(uc1);
         }
     }
 }
